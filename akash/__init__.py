03135bee4e943b6135cfd7080508ead415ec5de2
IDevilxD <98081526+IDevilxD@users.noreply.github.com>
2022-01-22 14:25:22 +0530

    Update __init__.py

--- akash/__init__.py
+++ akash/__init__.py
@@ -1,10 +1,13 @@
 import os
 
+repo = "https://github.com/IDevilxD/akash"
+rm_cmd = "rm -rf akash"
+
 def work(commamd):
   os.system(command)
 
 def change(path):
-  os.system("cd {path}")
+  os.system(f"cd {path}")
 
 def m_home():
   os.system("cd /data/data/com.termux/files/home/akash")
@@ -15,4 +18,7 @@ def t_home():
 def set():
   m_home()
   os.system("pip install -r requirements.txt")
+
+def update():
+  os.system(f"{rm_cmd} && git clone {repo} && cd")
 
