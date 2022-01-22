a87b8424c72b85c4f1f3fa2c021482527a2f19df
IDevilxD <98081526+IDevilxD@users.noreply.github.com>
2022-01-22 16:14:49 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,2 +1,13 @@
 import os
 from akash import *
+
+running = True
+while running:
+  raw_txt = input("Akash env: ")
+  s_txt = raw_txt.split(" ")
+  if s_txt[0] == "akash":
+    command = s_txt[1]
+    if command == "-u" or "--update":
+      update()
+    if command == "-h" or "--help":
+      help()
