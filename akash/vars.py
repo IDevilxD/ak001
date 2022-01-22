3725356ea35103063cff0c10a7c52100cf704119
IDevilxD <98081526+IDevilxD@users.noreply.github.com>
2022-01-22 16:22:37 +0530

    Update vars.py

--- akash/vars.py
+++ akash/vars.py
@@ -1,5 +1,9 @@
-help_txt = """"
+help_txt = """
+
+Akash help:-
+
 -u, --update                         update akash
 -s, --setup                          setup akash
 -g, --get <link> <file name>         get codes of website
+
 """
